b4064ce7528b189965625c9887b1f0d586d81ebd
Andy Prickett <andy@andyprickett.com>
2018-07-19 15:00:45 -0700

    Assignment 02 - Testing: completed

--- controllers/MenuController.js
+++ controllers/MenuController.js
@@ -50,6 +50,10 @@ module.exports = class MenuController {
   getContactCount() {
     return this.contacts.length;
   }
+  
+  remindMe() {
+    return 'Learning is a life-long pursuit';
+  }
 
   exit() {
     console.log("Thanks for using AddressBloc!");
